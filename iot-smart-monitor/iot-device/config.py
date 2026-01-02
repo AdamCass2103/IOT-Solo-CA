fb2140342fefb49eaae2d32d7d3e0a5fa19fd860
AdamCass2103 <D00265095@student.dkit.ie>
2026-01-02 14:38:18 +0000

    IOT Set up

---
+++ iot-smart-monitor/iot-device/config.py
@@ -0,0 +1,9 @@
+# iot-device/config.py
+
+# PubNub placeholder keys
+PUBNUB_PUBLISH_KEY = "demo-publish-key"
+PUBNUB_SUBSCRIBE_KEY = "demo-subscribe-key"
+
+# Channels
+MOTION_CHANNEL = "motion-sensor-channel"
+LED_CHANNEL = "led-channel"
